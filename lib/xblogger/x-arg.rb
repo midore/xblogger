f412169ace50630b648c9460cebfe01ed2994f8d
midore <midorex@gmail.com>
2011-07-28 10:02:49 +0900

    changed option
    def arg_keys
    getinfo, getentry

--- lib/xblogger/x-arg.rb
+++ lib/xblogger/x-arg.rb
@@ -1,7 +1,6 @@
 module Bblogger
 
   class CheckStart
-
     def initialize(arg)
       begin
         @err = false
@@ -21,6 +20,7 @@ module Bblogger
     end
 
     private
+
     def help
       arg_keys.values.sort_by{|v| v}.each{|x| print x, "\n"}
       return 'help'
@@ -28,32 +28,33 @@ module Bblogger
 
     def arg_keys
       {
-        'get'=>'1, Get entries. Example: --get 2010-01',
-        'doc'=>'2, Look XML data. Example: --doc draft.txt',
+        'getinfo'=>'0, Get information. Example: --getinfo',
+        'get'=>'1, Get Feeds. Example: --get 2010-01 | Blogger,Google',
+        'getentry'=>'1, Get entry. Example: --getentry 123456789 # edit-id',
+        'doc'=>'2, XML data. Example: --doc draft.txt',
         'post'=>'3, Post entry. Example: --post draft.txt',
         'update'=>'4, Update entry. Example: --update 2010-01-01-xxx.txt',
-        'del'=>'5, Delete entry. Example: --del 1111234567899999 # edit id',
+        'del'=>'5, Delete entry. Example: --del 123456789 # edit id',
         'delete'=>'6, Delete entry file. Example: --delete /path/to/file',
         'h'=>'Help: --h'
       }
     end
 
     def check_arg
-      err_no_str, err_no_file = "No option", "No file exist"
+      err_no_str, err_no_file = "No option", "Not found"
       @h.keys.each{|k| return @err = err_no_str unless arg_keys["#{k}"]}
       k = @h.keys[0]
       if k == :get
-        (@h[:get] == "--get") ? @h[:get] = Time.now().strftime("%Y-%m") : nil
-        return arg_keys['get'] unless /\d{4}\-\d{2}$/.match(@h[:get])
       elsif k == :del
         (@h[:del] == "--del") ? @err = "Error: edit id" : nil
         @err = "Error: edit id" if /\D/.match(@h[:del])
+      elsif k == :getinfo
+      elsif k == :getentry
       else
         return err_no_file unless File.exist?(@h[k]) and File.file?(@h[k])
       end
       return [@err, @h]
     end
-
   end
 
 end
